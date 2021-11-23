1caa4bbfd51ff3836deff3190b843d6d5d719bd0
nasiana <nasian.nazwani.ahmed@gmail.com>
2021-11-23 22:49:34 +0000

    Pushing lesson 6

--- L6/Practice_Youtube.py
+++ L6/Practice_Youtube.py
@@ -6,17 +6,43 @@ Practicisng the coding problem from Corey Schafer's YouTube channel on creating 
 class Sentence:
 
     # specified the type of data str is by :object
-    def __init__(self, str: object):
+    def __init__(self, str):
         self.str = str
-
-    def seperate_string(self):
-        super().__init__(str)
+        self.n = 0
         self.seperate = self.str.split()
+        print(self.seperate)
+        self.max_len = len(self.seperate)
+        print(self.max_len)
 
-    def
+    # you need the __iter__ and __next__ methods for an iterable
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.n >= self.max_len:
+            raise StopIteration
+
+        index = self.n
+        result = self.seperate[index]
+        self.n += 1
+        return result
+
+
+    # def seperate_string(self):
+    #     super().__init__(str)
+    #     self.seperate = self.str.split()
+    #     return self.seperate
+    #
+    # def my_gen(self):
+    #     n = len(self.seperate)
+    #     print(self.str[n])
+    #     yield n
+
 
 my_sentence = Sentence('This is a test')
 
+# my_sentence = Sentence.my_gen(my_sentence)
 # create a generator for this function
 for word in my_sentence:
     print(word)
