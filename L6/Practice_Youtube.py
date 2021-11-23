b41e8d5f7acb608e757288b4282d831c12182694
nasiana <nasian.nazwani.ahmed@gmail.com>
2021-11-23 23:11:17 +0000

    Updating Practice YouTube file

--- L6/Practice_Youtube.py
+++ L6/Practice_Youtube.py
@@ -2,19 +2,18 @@
 Practicisng the coding problem from Corey Schafer's YouTube channel on creating your own iterators
 '''
 
-# create class sentence
+# creating an iterable with a class
+
 class Sentence:
 
-    # specified the type of data str is by :object
-    def __init__(self, str):
-        self.str = str
+    # i originally used str as my second argument but this is bad practice as str is how you declare a string in python
+    def __init__(self, sentence):
+        self.sentence = sentence
         self.n = 0
-        self.seperate = self.str.split()
-        print(self.seperate)
+        self.seperate = self.sentence.split()
         self.max_len = len(self.seperate)
-        print(self.max_len)
 
-    # you need the __iter__ and __next__ methods for an iterable
+    # you need the __iter__ and __next__ methods to create an object that is an iterable
 
     def __iter__(self):
         return self
@@ -28,22 +27,28 @@ class Sentence:
         self.n += 1
         return result
 
+# creating an iterable with a generator function
 
-    # def seperate_string(self):
-    #     super().__init__(str)
-    #     self.seperate = self.str.split()
-    #     return self.seperate
-    #
-    # def my_gen(self):
-    #     n = len(self.seperate)
-    #     print(self.str[n])
-    #     yield n
+def my_gen(sentence):
+    n = 0
+    seperate = sentence.split()
+    max_len = len(seperate)
+    while n < max_len:
+        # yield is the keyword needed to make this a generator
+        # use of the yield gives this __iter__ and __next__ methods without having to write out all that ugly notation
+        yield seperate[n]
+        n += 1
 
-
+# iterable using the class method
 my_sentence = Sentence('This is a test')
 
-# my_sentence = Sentence.my_gen(my_sentence)
-# create a generator for this function
 for word in my_sentence:
     print(word)
 
+print('\n BREAK \n')
+
+# iterable using the generator method
+sentence_2 = my_gen('This is a test')
+
+for word in sentence_2:
+    print(word)
